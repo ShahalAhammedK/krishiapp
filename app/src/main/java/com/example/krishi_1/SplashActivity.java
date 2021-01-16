c13ecb514cf7eb9d8a7ff440552183570b88d0ab
Asus <shahalshalukolloli@gmail.com>
2021-01-16 15:27:50 +0530

    splash screen implemented

---
+++ app/src/main/java/com/example/krishi_1/SplashActivity.java
@@ -0,0 +1,26 @@
+package com.example.krishi_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity {
+    private static int TIME_OUT = 5000; //Time to launch the another activity
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        //sh0w activity for few seconds and  goto LoginActivity
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashActivity.this, LoginActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, TIME_OUT);
+    }
+}
